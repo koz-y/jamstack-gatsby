dbb283287ad1f69a8870f87b5471cf4ce452c019
koz-y <77233172+koz-y@users.noreply.github.com>
2022-03-10 18:11:45 +0900

    Topページ　雛形

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,109 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
+import { Row, Col, Card } from "react-bootstrap"
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+// import image from  "../components/iamge"
+import homgeImage from "../images/home.jpg"
+import jigyo1Img from "../images/jigyo1.jpg"
+import jigyo2Img from "../images/jigyo2.jpg"
+import jigyo3Img from "../images/jigyo3.jpg"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <Row style={{ maxWidth: `960px` }}>
+      <img src={homgeImage}></img>
+    </Row>
+
+    <Row>
+      <Col xs={12} md={6} style={{ maxWidth: `480px` }}>
+        <Row>
+          <Col>インフォメーション</Col>
+        </Row>
+        <Row>
+          <Col>インフォメーション 1</Col>
+        </Row>
+        <Row>
+          <Col>インフォメーション2</Col>
+        </Row>
+        <Row>
+          <Col>インフォメーション3</Col>
+        </Row>
+        <Row>
+          <Col>インフォメーション4</Col>
+        </Row>
+        <Row>
+          <Col>インフォメーション5</Col>
+        </Row>
+      </Col>
+      <Col xs={12} md={6} style={{ maxWidth: `480px` }}>
+        <Row>
+          <Col>IR</Col>
+        </Row>
+        <Row>
+          <Col>IR情報</Col>
+        </Row>
+        <Row>
+          <Col>社長ブログ</Col>
+        </Row>
+        <Row>
+          <Col>広告スペース</Col>
+        </Row>
+        <Row>
+          <Col>広告スペース</Col>
+        </Row>
+        <Row>
+          <Col>インフォメーション5</Col>
+        </Row>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col>事業内容</Col>
+    </Row>
+    <Row>
+      <Col>
+        <Row>
+          <Col md="4">
+            <Card>
+              <Card.Img src={jigyo1Img}></Card.Img>
+              <Card.Body>
+                <Card.Title>事業内容1</Card.Title>
+                <Card.Text>吾輩は猫である。名前はまだ無い。</Card.Text>
+              </Card.Body>{" "}
+            </Card>
+          </Col>
+
+          <Col md="4">
+            <Card>
+              <Card.Img src={jigyo2Img}></Card.Img>
+              <Card.Body>
+                <Card.Title>事業内容2</Card.Title>
+                <Card.Text>吾輩は猫である。名前はまだ無い。</Card.Text>
+              </Card.Body>{" "}
+            </Card>
+          </Col>
+
+          <Col md="4">
+            <Card>
+              <Card.Img src={jigyo3Img}></Card.Img>
+              <Card.Body>
+                <Card.Title>事業内容3</Card.Title>
+                <Card.Text>吾輩は猫である。名前はまだ無い。</Card.Text>
+              </Card.Body>{" "}
+            </Card>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col> 余白 </Col>
+    </Row>
   </Layout>
 )
 
